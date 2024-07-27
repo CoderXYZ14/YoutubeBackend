6aa0da1616d5ab5e9fcb1fb67122af033244e56c
Shahwaiz Islam <s1404islam@gmail.com>
2024-07-28 01:13:51 +0530

    Refresh access token route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   loginUser,
   logoutUser,
+  refreshAccessToken,
   registerUser,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -25,4 +26,5 @@ router.route("/register").post(
 );
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser); //verify jwt would give access of user to logout
+router.route("/refresh-token").post(refreshAccessToken);
 export default router;
